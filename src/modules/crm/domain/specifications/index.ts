dc92ed660bd8c21d01ddaccb43487a4af93f4dfd
MaarksN <130610135+MaarksN@users.noreply.github.com>
2026-07-20 10:15:57 -0300

    1

---
+++ src/modules/crm/domain/specifications/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @file index.ts
+ * @description Barrel export dos modulos de specifications.
+ * @module CRM/Index
+ * @layer Domain
+ * @architecture Clean Architecture + DDD
+ * @generated PROSPECTOR-ATLAS Fase 9 - CRM Domain
+ */
+
+
+export * from './ValidLeadSpecification';
+export * from './QualifiedLeadSpecification';
+export * from './LeadCanMovePipelineSpecification';
+export * from './CompanyCanReceiveLeadSpecification';
+export * from './ContactIsPrimarySpecification';
+export * from './OpportunityOpenSpecification';
+export * from './TaskOverdueSpecification';
+export * from './MeetingAvailableSpecification';
+export * from './ProposalValidSpecification';
+export * from './DealCloseSpecification';
